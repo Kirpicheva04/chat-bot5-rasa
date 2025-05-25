f5c7c5b79621d034c36603f652ae9bf881db89ca
Kirpicheva Daria <118064855+Kirpicheva04@users.noreply.github.com>
2025-05-25 18:03:18 +0300

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,19 +1,1 @@
-from actions.actions_time import ActionGetTime
-from actions.actions_weather import ActionGetWeather
-from actions.actions_calculate import ActionCalculate
-from actions.actions_web_search import ActionWebSearch
-from actions.actions_analyze_sentiment import ActionAnalyzeSentiment
-from actions.actions_joke import ActionRandomSong
-from actions.actions_remember_name import ActionRememberName
-from actions.actions_get_name import ActionGetName
 
-__all__ = [
-    "ActionGetTime",
-    "ActionGetWeather",
-    "ActionCalculate",
-    "ActionWebSearch",
-    "ActionAnalyzeSentiment",
-    "ActionRandomSong",
-    "ActionRememberName",
-    "ActionGetName",
-]
